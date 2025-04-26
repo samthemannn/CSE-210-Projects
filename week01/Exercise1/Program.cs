4a84eefaab7d17d7674728f1d1ad20ea3986fee5
samthemannn <oiyagatoun@gmail.com>
2025-04-26 12:47:28 -0600

    YUR

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,5 +7,9 @@ class Program
         Console.WriteLine("Input your first name");
         string firstName = Console.ReadLine();
         Console.WriteLine($"Your first name is: {firstName}");
+        Console.WriteLine("Input Your Last Name");
+        string lastName = Console.ReadLine();
+        Console .WriteLine($"Your last name is: {lastName}");
+        Console .WriteLine($"Your full name is {firstName} , {lastName}");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.IO.Compression;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Please enter your Grade");
+        
+        int userInput = int.Parse(Console.ReadLine());
+        Console.WriteLine (userInput);
+        if (userInput > 90)
+        {  
+            Console.WriteLine ("You have an A");
+        }
+        else if (userInput > 80)
+        {
+            Console.WriteLine("You have a B ");
+        }
+        else if (userInput > 70) 
+        {
+            Console.WriteLine ("You have a C");
+        }
+        else if (userInput >= 60)
+        {
+            Console.WriteLine ("You have a D");
+        } 
+        else 
+        {
+            Console.WriteLine ("You have a F");
+        }
+
     }
 }
