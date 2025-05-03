a1da9b480c5ba63fac1fd96c4bf50cd796c83af1
samthemannn <oiyagatoun@gmail.com>
2025-05-03 16:48:44 -0600

    CSE210 Week2

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Welcome to the Magic Number Game");
+        Console.WriteLine("What is the magic Number");
+        
+        int magic_number= int.Parse(Console.ReadLine());
+        Console.WriteLine($"You chose {magic_number} as your magic number " );
+        
+        Console.WriteLine("What is guess of the magic number");
+        Console.Clear();
+
+
+        int guess =0;
+        
+        
+        int guess_count =0;
+        Console.WriteLine("Please try and guess the number");
+
+        while(guess!= magic_number)
+        {
+            Console.WriteLine("Please enter your guess");
+            guess = int.Parse(Console.ReadLine());
+            guess_count++;
+
+            if (guess < magic_number)
+            {
+                Console.WriteLine($"The Guess {guess} is lower than the magic number") ;
+                Console.WriteLine("Please Try again");
+            }
+            else if (guess > magic_number)
+            {
+                Console.WriteLine($"The Guess {guess} is Higher than the magic number");
+                Console.WriteLine("Please Try again");
+            }
+            else
+            {
+                Console.WriteLine("WOOOOHOOO YOU GUESSED THE RIGHT NUMBER!!!!!!! :)");
+                Console.WriteLine($"Your Guess Count was {guess_count} guesses");
+            }
+
+        
+        }   
+
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int input;
+
+        Console.WriteLine("Enter Numbers (enter 0 to stop):");
+        
+        do
+        {
+            Console.Write("Enter any positive integer: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)  // 
+            {
+                numbers.Add(input);
+            }
+
+        } while (input != 0);
+
+       
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"Sum: {sum}");
+
+       
+        if (numbers.Count > 0)  // 
+        {
+            float average = (float)sum / numbers.Count;  
+            Console.WriteLine($"Average: {average}");
+        }
+
+       
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"Maximum: {max}");
     }
 }
